9b61e92a0d0121d7ac429c265341b7520534cdec
injecti0n <rootgebruiker@gmail.com>
2019-01-15 14:19:06 +0100

    updated

--- src/main/java/springcleaners/app/HouseApp.java
+++ src/main/java/springcleaners/app/HouseApp.java
@@ -1,5 +1,10 @@
 package springcleaners.app;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import springcleaners.appconfig.AppConfig;
+import springcleaners.services.interfaces.CleaningService;
 import springcleaners.tools.impl.Broom;
 import springcleaners.tools.impl.CleaningServiceImpl;
 import springcleaners.tools.impl.VacuumCleaner;
@@ -8,19 +13,27 @@ import springcleaners.tools.interfaces.CleaningTool;
 public class HouseApp {
 
 	public static void main(String[] args) {
-		CleaningTool broom = new Broom();
-		CleaningTool vacuum = new VacuumCleaner();
+		/*
+		 * CleaningTool broom = new Broom(); CleaningTool vacuum = new VacuumCleaner();
+		 * 
+		 * CleaningServiceImpl jill = new CleaningServiceImpl(); jill.setTool(broom);
+		 * CleaningServiceImpl jane = new CleaningServiceImpl(); jane.setTool(vacuum);
+		 * 
+		 * jill.clean(); jane.clean();
+		 */
+		///// springggggg
+
+		ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 		
-		CleaningServiceImpl jill = new CleaningServiceImpl();
-		jill.setTool(broom);
-		CleaningServiceImpl jane = new CleaningServiceImpl();
-		jane.setTool(vacuum);
+		
+		CleaningService jill = ctx.getBean("jill", CleaningService.class);
+		CleaningService jane = ctx.getBean("jane", CleaningService.class);
 		
 		jill.clean();
 		jane.clean();
+		ctx.close();
 		
 		
-
 	}
 
 }
--- src/main/java/springcleaners/appconfig/AppConfig.java
+++ src/main/java/springcleaners/appconfig/AppConfig.java
@@ -1,9 +1,42 @@
 package springcleaners.appconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springcleaners.services.interfaces.CleaningService;
+import springcleaners.tools.impl.Broom;
+import springcleaners.tools.impl.CleaningServiceImpl;
+import springcleaners.tools.impl.VacuumCleaner;
+import springcleaners.tools.interfaces.CleaningTool;
 
 @Configuration
 public class AppConfig {
+	
+	@Bean
+	public CleaningTool broom() {
+		return new Broom();
+	}
+	
+	@Bean
+	public CleaningTool vacuum() {
+		return new VacuumCleaner();
+	}
+	
+	
+	@Bean
+	public CleaningService jill() {
+		CleaningServiceImpl cs = new CleaningServiceImpl();
+		cs.setTool(broom());
+		return cs;
+	}
+	
+	@Bean
+	public CleaningService jane() {
+		CleaningServiceImpl cs = new CleaningServiceImpl();
+		cs.setTool(vacuum());
+		return cs;
+		
+	}
 	
 	
 
